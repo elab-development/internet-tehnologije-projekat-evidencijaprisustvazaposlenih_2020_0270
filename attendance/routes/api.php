196d03c41675839c2beb443220dbd61c65f5c928
Jovana0325 <jd20200325@student.fon.bg.ac.rs>
2024-07-08 11:39:19 +0200

    user kontroler

--- attendance/routes/api.php
+++ attendance/routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DeskController;
+use App\Http\Controllers\UserController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,6 +23,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+
+Route::get('/desks', [DeskController::class, 'index']);
+Route::get('/desks/{id}', [DeskController::class, 'show']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
+
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -27,6 +40,12 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', function (Request $request) {
         return auth()->user();
     });
+
+    Route::resource('/desks', DeskController::class)
+    ->only(['store', 'update', 'destroy']);
+
+    Route::resource('/categories', CategoryController::class)
+    ->only(['store', 'update', 'destroy']);
 
     
     Route::post('/logout', [AuthController::class, 'logout']);
