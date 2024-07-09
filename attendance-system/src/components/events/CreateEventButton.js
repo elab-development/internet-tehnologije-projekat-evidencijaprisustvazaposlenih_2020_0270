dcbb256b9ddf740fc96cc4219a2abe8130bc5836
olga-01 <olga.stefanovic2001@gmail.com>
2024-07-09 10:25:25 +0200

    search bar i event button

--- attendance-system/src/components/events/CreateEventButton.js
+++ attendance-system/src/components/events/CreateEventButton.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react';
+
+import GlobalContext from '../../context/GlobalContext';
 
 const CreateEventButton = () => {
-  return (
-    <div>CreateEventButton</div>
-  )
-}
+  const { setShowEventModal } = useContext(GlobalContext);
 
-export default CreateEventButton
+  return (
+    <div
+      onClick={() => setShowEventModal(true)}
+      className='border p-2 rounded-full flex cursor-pointer items-center shadow-md hover:shadow-2xl'
+    >
+      <span className='material-icons-outlined  text-gray-600 mx-2'>add</span>
+      <span>Create Event</span>
+    </div>
+  );
+};
+
+export default CreateEventButton;
