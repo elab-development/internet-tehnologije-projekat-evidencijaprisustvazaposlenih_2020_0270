00334eaed3f62f2b1f5346a968cc2cdda7d6a0b8
Jovana0325 <jd20200325@student.fon.bg.ac.rs>
2024-07-09 10:19:50 +0200

    kategorija

--- attendance-system/src/components/category/CreateCategory.js
+++ attendance-system/src/components/category/CreateCategory.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import GlobalContext from '../../context/GlobalContext';
 
-const CreateCategory = () => {
+const CreateCategoryButton = () => {
+  const { setShowCategoriesModal } = useContext(GlobalContext);
+
   return (
-    <div>CreateCategory</div>
-  )
-}
+    <div
+      onClick={() => setShowCategoriesModal(true)}
+      className='border mt-2 p-2 rounded-full flex cursor-pointer items-center shadow-md hover:shadow-2xl'
+    >
+      <span className='material-icons-outlined  text-gray-600 mx-2'>add</span>
+      <span>Create Category</span>
+    </div>
+  );
+};
 
-export default CreateCategory
+export default CreateCategoryButton;
